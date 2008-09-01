214455a27b1c8b6050c9945c86321493aca8dd06
Troy Stribling <troy.stribling@gmail.com>
2008-09-01 23:08:50 +0000

    debugging application skelton

---
+++ src/webgnosus_supervisor.erl
@@ -0,0 +1,69 @@
+%%%-------------------------------------------------------------------
+%%% webgnosus application supervisor
+%%%-------------------------------------------------------------------
+
+-module(webgnosus_supervisor).
+-behaviour(supervisor).
+
+%% api
+-export([
+         start/0,
+         start_in_shell_for_testing/0
+        ]).
+
+%% supervisor callbacks
+-export([
+         start_link/1, 
+         init/1
+        ]).
+
+%%====================================================================
+%% supervisor callbacks
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State}
+%% Description: Initialize server
+%%--------------------------------------------------------------------
+init([]) ->
+    %% Install tweeter error handler
+     gen_event:swap_handler(alarm_handler, {alarm_handler, swap}, {webgnosus_alarm_handler, webgnosus}),
+
+    {ok, {{one_for_one, 3, 10},
+	  [{tag1, 
+	    {laconica_interface, start_link, []},
+	    permanent, 
+	    10000, 
+	    worker, 
+	    [laconica_interface]}
+	  ]}}.
+
+%%--------------------------------------------------------------------
+%% Function: start_link(Args) -> Result
+%% Description: start server and spawn workers. called by application.
+%%--------------------------------------------------------------------
+start_link(Args) ->
+    supervisor:start_link({local,?MODULE}, ?MODULE, Args).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start()
+%% Description
+%%--------------------------------------------------------------------
+start() ->
+    spawn(fun() ->
+		  supervisor:start_link({local,?MODULE}, ?MODULE, _Arg = [])
+	  end).
+%%--------------------------------------------------------------------
+%% Function: start_in_shell_for_testing()
+%% Description: start supervsior and spawn workers when requested
+%% from erl shell.
+%%--------------------------------------------------------------------
+start_in_shell_for_testing() ->
+    {ok, Pid} = supervisor:start_link({local,?MODULE}, ?MODULE, _Arg = []),
+    unlink(Pid).
+
+
+
+
