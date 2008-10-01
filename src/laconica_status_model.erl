7797debf9b4b697862215cb73ba25cb2695628fc
Troy Stribling <troy.stribling@gmail.com>
2008-10-01 12:10:12 +0000

    added query to return first in list and beginning text priocessing modules.

--- src/laconica_status_model.erl
+++ src/laconica_status_model.erl
@@ -14,6 +14,8 @@
           count/0,
           last_by_site/2,
           list_last_by_site/2,
+          first/1,
+          list_first/1,
           key/1
        ]).
 
@@ -24,9 +26,9 @@
 %%====================================================================
 %% API
 %%====================================================================
-%%--------------------------------------------------------------------
-%% table methods
-%%--------------------------------------------------------------------
+%%>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+%% generic table methods
+%%>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 %% Func: create_tables/0
 %% Description: create application database tables
 %%--------------------------------------------------------------------
@@ -47,9 +49,9 @@ delete_table() ->
 clear_table() ->
     webgnosus_dbi:clear_table(laconica_statuses).
 
-%%--------------------------------------------------------------------
-%% row methods
-%%--------------------------------------------------------------------
+%%>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+%% generic row methods
+%%>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 %%--------------------------------------------------------------------
 %% Func: write/1
 %% Description: write specified record to database
@@ -79,7 +81,6 @@ find(all) ->
 %% Func: count/0
 %% Description: return row count
 %%--------------------------------------------------------------------
-%% count rows
 count() ->    
     {atomic, Val} = mnesia:transaction(
         fun() ->
@@ -87,20 +88,53 @@ count() ->
         end),
     Val.       
 
+%%>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+%% model row methods
+%%>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+%%--------------------------------------------------------------------
+%% Func: key/1
+%% Description: define model key
+%%--------------------------------------------------------------------
+%% find all models
+key({StatusId, UserId, SiteUrl}) ->
+    {StatusId, UserId, SiteUrl}.
+
+%%--------------------------------------------------------------------
+%% Func: first/1
+%% Description: sort models by key and return specifed number
+%%              with smallest value.
+%%--------------------------------------------------------------------
+%% count rows
+first(Count) ->  
+    
+    SortedQH = qlc:sort(qlc:q([X || X <- mnesia:table(laconica_statuses)]),
+                {order, 
+                    fun(Status1, Status2) ->
+                      order_by_status_id_ascending(Status1, Status2)
+                    end}),
+
+   % and run the query
+   {atomic, Val} = mnesia:transaction(
+        fun() ->
+           Cursor = qlc:cursor(SortedQH),
+           Result = qlc:next_answers(Cursor, Count),
+           qlc:delete_cursor(Cursor),
+           Result
+        end),
+    Val.       
+
 %%--------------------------------------------------------------------
 %% Func: last_by_status_id/2
 %% Description: sort models by key and return specifed number
-%%              with largest value.
+%%              with largest value for specified site.
 %%--------------------------------------------------------------------
 %% count rows
 last_by_site(Site, LastCount) ->  
     
     SortedQH = qlc:sort(qlc:q([X || X <- mnesia:table(laconica_statuses), X#laconica_statuses.site =:= Site]),
                 {order, 
-                    fun(Status1,Status2) ->
-                       {StatusId1, _, _} = Status1#laconica_statuses.status_id,
-                       {StatusId2, _, _} = Status2#laconica_statuses.status_id,
-                       StatusId1 > StatusId2
+                    fun(Status1, Status2) ->
+                      order_by_status_id_descending(Status1, Status2)
                     end}),
 
    % and run the query
@@ -113,12 +147,14 @@ last_by_site(Site, LastCount) ->
         end),
     Val.       
 
+%%>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+%% format output
+%%>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 %%--------------------------------------------------------------------
 %% Func: list_last_by_status_id/2
 %% Description: sort models by key and return specifed number
 %%              with largest value.
 %%--------------------------------------------------------------------
-%% count rows
 list_last_by_site(Site, LastCount) ->  
     
     Display = fun(Status) -> 
@@ -129,9 +165,37 @@ list_last_by_site(Site, LastCount) ->
     ok.
 
 %%--------------------------------------------------------------------
-%% Func: key/1
-%% Description: define model key
+%% Func: list_first/1
+%% Description: sort models by key and return specifed number
+%%              with smallest value.
 %%--------------------------------------------------------------------
-%% find all models
-key({StatusId, UserId, SiteUrl}) ->
-    {StatusId, UserId, SiteUrl}.
+list_first(Count) ->  
+    
+    Display = fun(Status) -> 
+        io:format("~p~n~p~n~p~n~n", [Status#laconica_statuses.screen_name, Status#laconica_statuses.created_at, Status#laconica_statuses.text])
+    end,
+    
+    [Display(S) || S <- first(Count)],
+    ok.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Func: order_by_status_id_descending/2
+%% Description: sort by descending status_id.
+%%--------------------------------------------------------------------
+order_by_status_id_descending(Status1, Status2) ->
+    {StatusId1, _, _} = Status1#laconica_statuses.status_id,
+    {StatusId2, _, _} = Status2#laconica_statuses.status_id,
+    StatusId1 > StatusId2.
+        
+%%--------------------------------------------------------------------
+%% Func: order_by_status_id_ascending/2
+%% Description: sort by descending status_id.
+%%--------------------------------------------------------------------
+order_by_status_id_ascending(Status1, Status2) ->
+    {StatusId1, _, _} = Status1#laconica_statuses.status_id,
+    {StatusId2, _, _} = Status2#laconica_statuses.status_id,
+    StatusId1 < StatusId2.
+        
