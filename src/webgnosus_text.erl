9a00cbc889bf9f3d4081eebb9ad639c7f47b3805
Troy Stribling <troy.stribling@gmail.com>
2008-10-11 03:28:28 +0000

    adding single quote and apostraphe processing

--- src/webgnosus_text.erl
+++ src/webgnosus_text.erl
@@ -81,20 +81,51 @@ pad_single_quotes(Doc) ->
 match_single_quote_pairs([], [], Doc) ->   
     Doc;
 
-match_single_quote_pairs([Rh | _Rt], [], Doc) ->   
+match_single_quote_pairs(_Right, [], Doc) ->   
+    Doc;
+
+match_single_quote_pairs([], _Left, Doc) ->   
     Doc;
 
 match_single_quote_pairs(Right, Left, Doc) ->   
     [Rh | Rt] = Right,
     [Lh | Lt]  = Left,
     if 
-        Rh < Lh ->
+        is_right_apostrophe(Rh, Lh) ->
             match_single_quote_pairs(Rt, Left, Doc);
+        is_left_apostrophe(Rh, Lt) ->
+            match_single_quote_pairs(Right, Lt, Doc);            
         true    ->
             match_single_quote_pairs(Rt, Lt, replace_at_position(Lh, " ' ", replace_at_position(Rh, " ' ", Doc)))
     end.
     
 
+%%--------------------------------------------------------------------
+%% Func: is_right_apostrophe/2
+%% Description: true if Rh is an apostrophe.
+%%--------------------------------------------------------------------
+is_right_apostrophe({RhPos, _}, {LhPos, _}) ->   
+    if
+        RhPos < LhPos -> 
+            true;
+        true -> 
+            false
+    end.
+
+%%--------------------------------------------------------------------
+%% Func: is_left_apostrophe/3
+%% Description: true if Lh is an apostrophe.
+%%--------------------------------------------------------------------
+is_left_apostrophe(_Rh, []) ->   
+    false;
+
+is_left_apostrophe({RhPos, _}, [{Lh2Pos} | _]) ->   
+    if
+        Lh2Pos < RhPos -> 
+            true;
+        true -> 
+            false
+    end.
 
 %%--------------------------------------------------------------------
 %% Func: remove_new_lines/1
