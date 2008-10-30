fa8224d35c9a72814e7906d6f0dec0968c33dd24
Troy Stribling <troy.stribling@gmail.com>
2008-10-30 01:12:17 +0000

    finished pos tagger coding.

--- scripts/pos_tagger.rb
+++ scripts/pos_tagger.rb
@@ -4,14 +4,21 @@ def read_file (file)
         while row = f.gets
             yield row
         end
+        f.close()
     end
 end
 
-#### run tagger and return output
-def run_tagger()
-end
-
 #### run program
+out_file = File.open(ARGV.last, "w")
 read_file(ARGV.first) do |row|
-  puts row
+    puts "PROCESSING: #{row}"
+    File.open("tmp_in.dat", "w+") {|f| f.write(row); f.close()}
+    `java -mx300m -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -model models/bidirectional-wsj-0-18.tagger -prop myPropsFile.prop -textFile tmp_in.dat > tmp_out.dat`
+    File.open("tmp_out.dat", "r") do |f| 
+        tagged = f.readlines(row)
+        f.close()
+        puts "TAGGED: #{tagged}"
+        out_file.write(tagged)
+    end
 end
+out_file.close()
