ce2c1d761ba4d079a2c2ec65e8cff063e2f20493
Troy Stribling <troy.stribling@gmail.com>
2008-10-31 00:15:21 +0000

    increased jvm  memory to 500mb

--- scripts/pos_tagger.rb
+++ scripts/pos_tagger.rb
@@ -13,7 +13,7 @@ out_file = File.open(ARGV.last, "w")
 read_file(ARGV.first) do |row|
     puts "PROCESSING: #{row}"
     File.open("tmp_in.dat", "w+") {|f| f.write(row); f.close()}
-    `java -mx300m -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -model models/bidirectional-wsj-0-18.tagger -prop myPropsFile.prop -textFile tmp_in.dat > tmp_out.dat`
+    `java -mx500m -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -model models/bidirectional-wsj-0-18.tagger -prop myPropsFile.prop -textFile tmp_in.dat > tmp_out.dat`
     File.open("tmp_out.dat", "r") do |f| 
         tagged = f.readlines(row)
         f.close()
